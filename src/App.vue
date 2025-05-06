a0808c8c3f0869a1570ac630f084c13378b424fb
flycran <cffhidol@163.com>
2025-05-07 04:31:21 +0800

    修改重定向逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-if(location.pathname === '/') {
-  router.replace('/data')
-}
 </script>
 
 <template>
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+router.replace('/data')
+</script>
+
+<template>
+  <div class="pages">Pages</div>
+</template>
+
+<style lang="scss" scoped>
+</style>
